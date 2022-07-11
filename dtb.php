d96f154972b73af77750bff128c58c6c4cd6a4ad
Uric01 <uric01@live.com>
2022-07-11 07:53:11 +0200

    Products line iterator

--- dtb.php
+++ dtb.php
@@ -18,7 +18,7 @@
         }
 
 
-        $sql = "SELECT * FROM invoice";
+        $sql = "SELECT * FROM invoice WHERE invoiceID=1";
 
         $result = $conn->prepare($sql);
        /* $a = 1;
@@ -26,4 +26,76 @@
 
         $result->execute();
 
-        $row = $result->fetch(\PDO::FETCH_ASSOC);
+        $row = $result->fetchAll(\PDO::FETCH_ASSOC);
+
+        //print_r($row[0]['clientID']);
+
+        $invoiceRef = $row[0]['invoiceID'];
+        $date = $row[0]['date'];
+        $clientID = $row[0]['clientID'];
+       
+        $vat = 0;
+        $total = 0;
+
+
+        $arr[] = "";
+
+        $lineCount = 0;
+        $lineTotal = 0;
+        
+
+        $arr = json_decode($row[0]['item'], true);
+
+       /* 
+       // print_r($arr[0]['product']);
+        echo "<pre>";
+        print_r($arr);
+        echo "<pre/>";
+
+
+        echo count($arr[0])."<br/>";
+
+
+        for($i = 0; $i < count($arr); $i++)
+        {
+
+            echo "Quantity: " . $arr[$i]['qty']."<br/>";
+            echo "SKU: " .$arr[$i]['SKU']."<br/>";
+            echo "Product: " .$arr[$i]['product']."<br/>";
+            echo "Price ex vat: " .$arr[$i]['price']."<br/><br/>";
+           
+        }
+      
+
+    /*foreach ($row as $rows){
+
+
+    $lineCount +=5;
+    
+    $arr = json_decode($rows['item'], true);
+
+    
+    $priceExVat = ($rows['amount']); //single product amount  
+    $qty = $arr['qty'];
+    $lineTotal = $rows['amount'] * $qty;
+
+    $total += $lineTotal;
+
+    $x = 0;
+foreach ($arr as  $data[]){ // products iterator
+
+
+    echo "Quantity: " . $data['qty']."<br/>";
+    echo "SKU: " .$data[1]."<br/>";
+    echo "Product: " .$data[2]."<br/>";
+    echo "Price ex vat: " .$data['priceExVat']. "<br/>";
+}
+
+//print_r($arr);
+
+}*/
+
+$vat = ($total *1.15) - $total;
+$total += $vat;
+/*echo "VAT: " ."R".round( $vat)."<br/>";
+echo "Total: " ."R".round( $total)."<br/>";*/
--- index.php
+++ index.php
@@ -8,13 +8,6 @@ require('fpdf184/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 
-$lineCount = 0;
-
-$invoiceRef = $row['invoiceID'];
-$date = $row['date'];
-$clientID = $row['clientID'];
-$vat = 0;
-$total = 0;
 
 
 $pdf->SetFont('Arial', '', 12);
@@ -24,10 +17,10 @@ $pdf->Ln(25);
 $pdf->cell(20, 5, 'Date', 0, 0);
 $pdf->cell(99, 5, ': '.$date, 0, 0);
 $pdf->cell(39, 5, 'Tax Invoice', 0, 0);
-$pdf->cell(39, 5, ': '.$invoiceRef, 0, 1);
+$pdf->cell(39, 5, ': #'.$invoiceRef, 0, 1);
 $pdf->SetFont('Arial', '', 35);
 $pdf->cell(120, 20, '', 0, 0);
-$pdf->cell(39, 20,  'R'.round($total,2), 0, 1);
+$pdf->cell(39, 20,  'R '.round($total,2), 0, 1);
 $pdf->Ln(10);
 $pdf->SetFont('Arial', '', 12);
 $pdf->cell(120, 5, 'Tax invoice to:', 0, 0, 'L');
@@ -59,62 +52,54 @@ $pdf->cell(40, 5, 'Price ex vat', 0, 0);
 $pdf->cell(26, 5, 'Line Total', 0, 1);
 $pdf->Line(12, 115, 197, 115);
 
-$arr[] = "";
-foreach ($row as $rows){
 
-    $lineCount +=5;
-    
-    $arr = json_decode($rows['item'], true);
-    
-    $priceExVat = ($rows['amount']/1.15); //single product amount 
+$x = 0;
 
-    $qty = $rows['qty'];
-    $lineTotal = $priceExVat * $qty;
+for($i = 0; $i < count($arr); $i++)
+{
 
-    $vat += (($lineTotal * $qty)/0.85) - $lineTotal;
-    $total += $vat + $lineTotal;
 
-    $pdf->cell(22, 5, $qty, 0, 0);
-    $pdf->cell(34, 5, $arr['SKU'], 0, 0);
-    $pdf->cell(60, 5, $arr['product'], 0, 0);
-    $pdf->cell(40, 5, 'R'.round($priceExVat,2), 0, 0);
-    $pdf->cell(26, 5, 'R'.round($lineTotal,2), 0, 1);
-
+    $pdf->cell(22, 5,  $arr[$i]['qty'], 0, 0, 'C');
+    $pdf->cell(34, 5,$arr[$i]['SKU'], 0, 0);
+    $pdf->cell(60, 5, $arr[$i]['product'], 0, 0);
+    $pdf->cell(40, 5, 'R '.round($arr[$i]['price'],2), 0, 0);
+    $pdf->cell(26, 5, 'R '.round($arr[$i]['price']*$arr[$i]['qty'],2), 0, 1);
+   
+$x += $i * 2;
 
 }
 
 
-
-$pdf->Line(12, 200, 197, 200);
-$pdf->Ln(80);
-$pdf->cell(120, 10, '', 0, 0);
-$pdf->cell(40, 10, 'VAT', 0, 0);
-$pdf->cell(26, 10, 'R'.round($vat,2), 0, 1);
-$pdf->cell(120, 10, '', 0, 0);
-$pdf->cell(40, 10, 'TOTAL', 0, 0);
-$pdf->cell(26, 10, 'R'.round($total,2), 0, 1);
-$pdf->Line(10, 220, 200, 220);
-$pdf->Ln(2);
-$pdf->cell(123, 5, 'PAYMENT DETAILS:', 0, 0, 'L');
-$pdf->cell(39, 5, 'OTHER INFORMATION', 0, 1);
-$pdf->cell(39, 5, 'Name of beneficiary', 0, 0);
-$pdf->cell(84, 5, ': Brand Hubb', 0, 0);
-$pdf->cell(40, 5, 'Shop Acer', 0, 1);
-$pdf->cell(39, 5, 'Name of Bank', 0, 0);
-$pdf->cell(84, 5, ': Standard Bank', 0, 0);
-$pdf->cell(39, 5, 'Phone', 0, 0);
-$pdf->cell(30, 5, ': 010 020 8606', 0, 1);
-$pdf->cell(39, 5, 'Address of Bank', 0, 0);
-$pdf->cell(84, 5, ': Sunninghill', 0, 0);
-$pdf->cell(39, 5, 'www.shopacer.co.za', 0, 1);
-$pdf->cell(39, 5, 'Account Number', 0, 0);
-$pdf->cell(84, 5, ': 35-28-546-1', 0, 0);
-$pdf->cell(39, 5, 'zweli@shopacer.co.za', 0, 1);
-$pdf->cell(39, 5, 'SWIFT Code', 0, 0);
-$pdf->cell(39, 5, ': SB-ZA-ZA-JJ', 0, 1);
-$pdf->cell(39, 5, 'Payment reference', 0, 0);
-$pdf->cell(84, 5, ': #20220704AZ1', 0, 0);
-$pdf->Line(10, 259, 200, 259);
+$pdf->Line(12, 200+$x, 197, 200+$x);
+$pdf->Ln(80+$x);
+$pdf->cell(120, 10+$x, '', 0, 0);
+$pdf->cell(40, 10+$x, 'VAT', 0, 0);
+$pdf->cell(26, 10+$x, 'R '.round($vat,2), 0, 1);
+$pdf->cell(120, 10+$x, '', 0, 0);
+$pdf->cell(40, 10+$x, 'TOTAL', 0, 0);
+$pdf->cell(26, 10+$x, 'R '.round($total,2), 0, 1);
+$pdf->Line(10, 220+$x, 200, 220+$x);
+$pdf->Ln(2+$x);
+$pdf->cell(123, 5+$x, 'PAYMENT DETAILS:', 0, 0, 'L');
+$pdf->cell(39, 5+$x, 'OTHER INFORMATION', 0, 1);
+$pdf->cell(39, 5+$x, 'Name of beneficiary', 0, 0);
+$pdf->cell(84, 5+$x, ': Brand Hubb', 0, 0);
+$pdf->cell(40, 5+$x, 'Shop Acer', 0, 1);
+$pdf->cell(39, 5+$x, 'Name of Bank', 0, 0);
+$pdf->cell(84, 5+$x, ': Standard Bank', 0, 0);
+$pdf->cell(39, 5+$x, 'Phone', 0, 0);
+$pdf->cell(30, 5+$x, ': 010 020 8606', 0, 1);
+$pdf->cell(39, 5+$x, 'Address of Bank', 0, 0);
+$pdf->cell(84, 5+$x, ': Sunninghill', 0, 0);
+$pdf->cell(39, 5+$x, 'www.shopacer.co.za', 0, 1);
+$pdf->cell(39, 5+$x, 'Account Number', 0, 0);
+$pdf->cell(84, 5+$x, ': 35-28-546-1', 0, 0);
+$pdf->cell(39, 5+$x, 'zweli@shopacer.co.za', 0, 1);
+$pdf->cell(39, 5+$x, 'SWIFT Code', 0, 0);
+$pdf->cell(39, 5+$x, ': SB-ZA-ZA-JJ', 0, 1);
+$pdf->cell(39, 5+$x, 'Payment reference', 0, 0);
+$pdf->cell(84, 5+$x, ': #20220704AZ1', 0, 0);
+$pdf->Line(10, 259+$x, 200, 259+$x);
 
 
 // Position at 4.1 cm from bottom
@@ -123,7 +108,7 @@ $pdf->SetY(-41);
 $pdf->SetFont('Arial', 'I', 9);
 // Text color in gray
 $pdf->SetTextColor(128);
-$pdf->Cell(0, 10, 'If you have any questions concerning this tax invoice, Contact us on support@shopacer.co.za or 010 020 8606', 0, 1, 'C');
+$pdf->Cell(0, 10+$x, 'If you have any questions concerning this tax invoice, Contact us on support@shopacer.co.za or 010 020 8606', 0, 1, 'C');
 $pdf->SetFont('Arial', 'I', 8);
 // Page number
 $pdf->Cell(0, 10, 'Page ' . $pdf->PageNo(), 0, 0, 'C');
