2a18597fdd50111b863cf1600e4d1e47bc0a6803
Uric01 <uric01@live.com>
2022-07-13 00:32:23 +0200

    UI Design

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Invoice</title>
+</head>
+<body>
+    
+<div class="container">
+    <img src="logo.png" alt="Logo" class="logo">
+    <hr>
+
+    <div class="date">
+            Date:
+    </div>
+
+    <div class="tax-invoice">
+        Tax Invoice
+    </div>
+
+    <div class="total">
+        Total Amount
+    </div>
+
+    <div class="invoice-to">
+            To
+    </div>
+
+
+    <div class="invoice-from">
+   
+        From
+    </div>
+
+    <hr>
+
+    <ul>
+        <li>Qty</li>
+        <li>SKU</li>
+        <li>Description</li>
+        <li>Price Ex Vat</li>
+        <li>Line Total</li>
+    </ul>
+
+    <hr>
+
+    <div class="total-vat">
+   
+   VAT<br/>
+   Total
+    </div>
+    <hr>
+
+    <div class="bank">
+        Bank Details
+    </div>
+
+    <div class="info">
+    More Info
+   From
+</div>
+<hr>
+</div>
+
+</body>
+</html>
